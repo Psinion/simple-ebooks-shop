bbbcd3a02331285180b2315229f055ebfd19e289
Mosalev Daniel <mosalev.daniel@gmail.com>
2024-04-24 23:00:13 +0700

    Create backend solution

---
+++ src/ServerApp/SimpleEbookShop/SES.WebApi/StartupConfiguration/ServicesExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace SES.Application.StartupConfiguration;
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ src/ServerApp/SimpleEbookShop/SES.WebApi/StartupConfiguration/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace SES.Application.StartupConfiguration;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { 
+                Title = "SimpleEbookShop",
+                Version = "v1" 
+            });
+        });
+        
+        /*services.AddDbContext<MainDbContext>(options =>
+            options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
+        );*/
+
+        services.AddControllersWithViews()
+            .AddJsonOptions(options =>
+            {
+                options
+                    .JsonSerializerOptions
+                    .ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+
+        services
+            .AddRepositories();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Solar");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseCors();
+
+        app.UseEndpoints(endpoints =>
+            endpoints.MapControllers()
+        );
+    }
+}
